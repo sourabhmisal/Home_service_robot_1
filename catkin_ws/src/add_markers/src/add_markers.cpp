4b5b940fcba94e4b3e9c09defc8299cced57d5f3
root <Sourabh Misal>
2020-05-02 18:42:03 +0000

    Corrected code of pickup and dropoff nodes

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -1,41 +1,36 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <std_msgs/Int32.h>
 #include <nav_msgs/Odometry.h>
 #include <math.h>
 
 double pickUpX = 6;
 double pickUpY = 0;
 double dropOffX = 0;
-double dropOffY = -7;
+double dropOffY = -6;
 
-bool pickingUp = false;
-bool droppingOff = false;
-bool pickedUp = false;
-bool droppedOff = false;
+enum action {goingToPickUp = 1, pickedUp = 2, goingToDropOff = 3, droppedOff = 4};
+int state = 0;
 
-
-void odometryCallBack (const nav_msgs::Odometry::ConstPtr& msg)
+void statusChange(const std_msgs::Int32::ConstPtr& msg)
 {
-  double robotX = msg->pose.pose.position.x;
-  double robotY = msg->pose.pose.position.y;
-
-  double distance = 0.0;
-  if(!pickingUp){
-    distance = sqrt(pow((robotX - pickUpX), 2) + pow((robotY - pickUpY), 2));
-    ROS_INFO("Pick up zone distance = %f", distance);
-    if(distance <= 0.5){
-      ROS_INFO("In the pick up area");
-      pickingUp = true;
-    }
-  }
-  else if(!droppingOff){
-    distance = sqrt(pow((robotX - dropOffX), 2) + pow((robotY - dropOffY), 2));
-    ROS_INFO("Drop zone distance = %f", distance);
-    if(distance <= 0.5){
-      ROS_INFO("In the drop off area");
-      droppedOff = true;
-    }
-  }    
+  ROS_INFO("Status Changed!");
+  switch(msg->data)
+  {
+    case goingToPickUp:
+      ROS_INFO("Status: On way to pick up");
+      break;
+    case pickedUp:
+      ROS_INFO("Status: Picked up item");
+      break;
+    case goingToDropOff:
+      ROS_INFO("Status: On way to drop off");
+      break;
+    case droppedOff:
+      ROS_INFO("Status: Dropped off item");
+      break;
+  } 
+  state = msg->data;
 }
 
 int main(int argc, char**argv)
@@ -44,7 +39,7 @@ int main(int argc, char**argv)
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker",1);
-  ros::Subscriber odom_sub = n.subscribe("amcl_pose",10,odometryCallBack);
+  ros::Subscriber status_sub = n.subscribe("robot_status",1,statusChange);
 
   visualization_msgs::Marker marker;
   
@@ -58,10 +53,6 @@ int main(int argc, char**argv)
 
   marker.type = shape;
   
-  marker.action = visualization_msgs::Marker::ADD;
-  
-  marker.pose.position.x = pickUpX;
-  marker.pose.position.y = pickUpY;
   marker.pose.position.z = 0.05;
   marker.pose.orientation.x = 0;
   marker.pose.orientation.y = 0;
@@ -89,18 +80,28 @@ int main(int argc, char**argv)
       }
       ROS_WARN("Please create a subscriber to the marker");
       sleep(1);
-    }  
-    if (pickingUp && !pickedUp)
-    {
-      pickedUp = true;
-      marker.action = visualization_msgs::Marker::DELETE;
     }
-    if (droppingOff && !droppedOff)
+
+    switch(state)
     {
-      marker.pose.position.x = dropOffX;
-      marker.pose.position.y = dropOffY;
-      marker.action = visualization_msgs::Marker::ADD;
-    } 
+      case goingToPickUp:
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = pickUpX;
+        marker.pose.position.y = pickUpY;
+        break;
+      case pickedUp:
+        marker.action = visualization_msgs::Marker::DELETE;
+        break;   
+      case goingToDropOff:
+        marker.action = visualization_msgs::Marker::DELETE;
+        break;
+      case droppedOff:
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = dropOffX;
+        marker.pose.position.y = dropOffY;
+        break;
+    }  
+
     marker_pub.publish(marker);
     ros::spinOnce();
   }
--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <std_msgs/Int32.h>
 #include <actionlib/client/simple_action_client.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -7,20 +8,25 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 double pickUpX = 6;
 double pickUpY = 0;
 double dropOffX = 0;
-double dropOffY = -7;
+double dropOffY = -6;
 
+enum action {goingToPickUp = 1, pickedUp = 2, goingToDropOff = 3, droppedOff = 4};
 
 int main(int argc, char** argv){
 
   ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
 
   MoveBaseClient ac("move_base", true);
+  
+  ros::Publisher status_pub = n.advertise<std_msgs::Int32>("robot_status", 1);
   
   while(!ac.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for move_base action server to come up");
   }
 
   move_base_msgs::MoveBaseGoal goal;
+  std_msgs::Int32 statusMsg;
 
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp =  ros::Time::now();
@@ -30,32 +36,43 @@ int main(int argc, char** argv){
   goal.target_pose.pose.orientation.w = 1.0;
 
   ROS_INFO("Sending pick up point");
+  statusMsg.data = goingToPickUp;
+  status_pub.publish(statusMsg);
   ac.sendGoal(goal);
 
   ac.waitForResult();
 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  {  
     ROS_INFO("Hurray, the robot reached pick up point");
+    statusMsg.data = pickedUp;
+    status_pub.publish(statusMsg);
+  }
   else
     ROS_INFO("The base failed to reach pick up point for some reason");
 
-  ros::Duration(5).sleep();
+  sleep(5);
 
   goal.target_pose.pose.position.x = dropOffX;
   goal.target_pose.pose.position.y = dropOffY;
   goal.target_pose.pose.orientation.w = 1.0;
 
   ROS_INFO("Sending drop off point");
+  statusMsg.data = goingToDropOff;
+  status_pub.publish(statusMsg);
   ac.sendGoal(goal);
 
   ac.waitForResult();
 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hurray, the robot reached drop off point");
+  {  ROS_INFO("Hurray, the robot reached drop off point");
+     statusMsg.data = droppedOff;
+     status_pub.publish(statusMsg);
+  }
   else
     ROS_INFO("The base failed to reach drop off point for some reason");
-
+  
+  sleep(5);
   return(0); 
 
 }
- 
